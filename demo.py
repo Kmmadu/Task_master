88ef58c6673054e9fe28a0b9e6c381ae28af40c3
kmmadu <mmadubugwukingsley@gmail.com>
2023-11-16 03:03:04 +0100

    Almost done with 'Add Task' function

--- demo.py
+++ demo.py
@@ -1,4 +1,26 @@
-friends = ["chika","danile","toby"]
+# Menu for the user
+print("Task List Manager")
+print("\n1. Add Task")
+print("2. View Tasks")
+print("3. Mark Task as Completed")
+print("4. Delete Task")
+print("5. Quit")
 
-for friend in friends:
-    print(friend.title())
+# This is an empty list, to house the user task
+task_list = []
+compl_task = []
+
+# This is the input message
+input_message = input("Enter your choice: ")
+
+#Convented input_message to int 
+input_message = int(input_message)
+
+# Just declare the active for the WHILE loop
+active = True
+
+# loop for the task
+while active:
+    if input_message == 1:
+        get_task = input("Enter task name: ")
+        task_list = task_list.append(get_task)
