bc6e3ec8d9f5f3749c719fb618fb55f05ceeb538
kmmadu <mmadubugwukingsley@gmail.com>
2023-11-16 08:24:57 +0100

    I just modified Add Task

--- demo.py
+++ demo.py
@@ -4,23 +4,25 @@ print("\n1. Add Task")
 print("2. View Tasks")
 print("3. Mark Task as Completed")
 print("4. Delete Task")
-print("5. Quit")
+print("5. Quit\n")
 
 # This is an empty list, to house the user task
-task_list = []
+task_list = {}
 compl_task = []
-
-# This is the input message
-input_message = input("Enter your choice: ")
-
-#Convented input_message to int 
-input_message = int(input_message)
 
 # Just declare the active for the WHILE loop
 active = True
 
 # loop for the task
 while active:
+
+    # This is the input message
+    input_message = input("Enter your choice: ")
+
+    #Convented input_message to int 
+    input_message = int(input_message)
+
     if input_message == 1:
         get_task = input("Enter task name: ")
-        task_list = task_list.append(get_task)
+        due_date = input("Enter due date (DD-MM-YYYY): ")
+        due_date = str (due_date)
